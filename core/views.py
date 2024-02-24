88950c569b8d670ea2f8be579633289c54ca3960
FlakoBB <josephrbb99@gmail.com>
2024-02-24 15:46:04 -0600

    add: calculate totals and available

--- core/views.py
+++ core/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from transactions.models import Income, Expense
 
 def index(request):
-  return render(request, 'core/index.html')
+  incomes = Income.objects.values_list('amount', flat=True)
+
+  total_income = 0
+  for amount in incomes:
+    total_income += amount
+
+  expenses = Expense.objects.values_list('amount', flat=True)
+
+  total_expense = 0
+  for amount in expenses:
+    total_expense += amount
+
+  available = total_income - total_expense
+
+  context = {
+    'total_income': total_income,
+    'total_expense': total_expense,
+    'available': available,
+  }
+
+  return render(request, 'core/index.html', context)
