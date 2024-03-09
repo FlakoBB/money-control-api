26492121d78177960ab7d153a231666696168fe7
FlakoBB <josephrbb99@gmail.com>
2024-03-09 17:14:04 -0600

    add ExpenseCategory model

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Income)
+admin.site.register(ExpenseCategory)
+admin.site.register(Expense)
--- transactions/models.py
+++ transactions/models.py
@@ -14,7 +14,19 @@ class Income(models.Model):
 
   def __str__(self):
     return f'{self.title}: ${self.amount}'
-  
+
+class ExpenseCategory(models.Model):
+  title = models.CharField(
+    max_length=100,
+  )
+  description = models.TextField(
+    null=True,
+    blank=True,
+  )
+
+  def __str__(self):
+    return self.title
+
 class Expense(models.Model):
   title = models.CharField(
     max_length=100,
@@ -25,6 +37,19 @@ class Expense(models.Model):
   )
   date = models.DateTimeField(
     auto_now_add=True,
+  )
+  taken_from = models.ForeignKey(
+    'Income',
+    on_delete=models.CASCADE,
+  )
+  is_necessary = models.BooleanField(
+    default=False,
+  )
+  category = models.ForeignKey(
+    'ExpenseCategory',
+    null=True,
+    blank=True,
+    on_delete=models.SET_NULL,
   )
 
   def __str__(self):
