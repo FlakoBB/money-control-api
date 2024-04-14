9a88af4aea570d615d2d26eca4f1262110683aa9
FlakoBB <josephrbb99@gmail.com>
2024-04-14 14:35:17 -0600

    feat: saving allocation and withdraw

--- savings/urls.py
+++ savings/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
   path('all/', views.saving_list, name='savings'),
   path('create/', views.create_saving, name='create_saving'),
-  path('details/<int:saving_id>', views.saving_details, name='saving_details'),
+  path('details/<int:saving_id>/', views.saving_details, name='saving_details'),
+  path('<int:saving_id>/add/', views.add_saving_funds, name='add_saving_funds'),
+  path('<int:saving_id>/withdraw/', views.withdraw_from_saving, name='withdraw_from_saving'),
 ]
--- savings/views.py
+++ savings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Saving
+from .models import Saving, SavingAllocation, SavingWithdrawal
 
 def saving_list(request):
   user = request.user
@@ -45,3 +45,51 @@ def saving_details(request, saving_id):
   }
 
   return render(request, 'savings/saving_details.html', context)
+
+def add_saving_funds(request, saving_id):
+  user = request.user
+  saving = Saving.objects.get(id=saving_id)
+
+  context = {
+    'saving_title': saving.title,
+  }
+
+  if request.method == 'POST':
+    form = request.POST
+
+    amount = int(form['amount']) if form['amount'] != '' else ''
+
+    if amount != '':
+      SavingAllocation.objects.create(user=user, saving=saving, amount=amount)
+      return redirect('saving_details', saving_id)
+
+    else:
+      context['amount_error'] = 'Introduzca una cantidad valida.'
+
+      return render(request, 'savings/add_saving_fund.html', context)
+  
+  return render(request, 'savings/add_saving_fund.html', context)
+
+def withdraw_from_saving(request, saving_id):
+  user = request.user
+  saving = Saving.objects.get(id=saving_id)
+
+  context = {
+    'saving_title': saving.title,
+  }
+
+  if request.method == 'POST':
+    form = request.POST
+
+    amount = int(form['amount']) if form['amount'] != '' else ''
+
+    if amount != '':
+      SavingWithdrawal.objects.create(user=user, saving=saving, amount=amount)
+      return redirect('saving_details', saving_id)
+
+    else:
+      context['amount_error'] = 'Introduzca una cantidad valida.'
+
+      return render(request, 'savings/withdraw_from_saving.html', context)
+  
+  return render(request, 'savings/withdraw_from_saving.html', context)
